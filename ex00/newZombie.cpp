48103b42de2a22b88ec77f5c834681423f94caf3
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-02-14 16:29:02 +0100

    to be fixed later

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 13:30:39 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/08 13:30:40 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/02/14 16:19:07 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include "Zombie.hpp"
 
 
-
-Zombie* newZombie( std::string name )
+Zombie* Zombie::newZombie( std::string name )
 {
     Zombie * zombie= new Zombie(name);
     return (zombie);
