892ea515a585cd3180b8c6f7ac4e45bcd797406d
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-02-17 22:14:41 +0100

    refactor main

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,13 +6,13 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 19:02:14 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/17 21:24:19 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/02/17 22:06:59 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 
-#include <Harl.hpp>
+#include "Harl.hpp"
 
 
 int main (int ac, char **av)
@@ -21,9 +21,7 @@ int main (int ac, char **av)
     Harl harl;
 
     if (ac == 2)
-    {
         harl.complain(av[1]); 
-    }
     else 
     {
         std::cout << "Valid inputs are => [DEBUG|INFO|WARNING|ERROR]" << std::endl;
