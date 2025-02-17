a3574610171f16c0e1fd627be614668addf55666
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-02-17 22:11:41 +0100

    Sad :( -Werror=implicit-fallthrough, yet i have to use switches

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 19:06:35 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/17 21:22:05 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/02/17 22:10:03 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,25 +49,23 @@ void Harl::complain(std::string level)
     void (Harl::*ptrs[4])() = {&Harl::debug, &Harl::error, &Harl::warning, &Harl::info}; 
 
     size_t i = 0; 
-    while (i < 4)
-    {
-        if (values[i] == level)
-            return ;
-        i++;
-    }
+    while (i < 4 && values[i] != level)
+        i++; 
     switch (i)
     {
-    case 0:
-        (this->*ptrs[0])();
-    case 1:
-        (this->*ptrs[1])();
-    case 2:
-        (this->*ptrs[2])();
-    case 3:
-        (this->*ptrs[3])();
-        break;
-    default:
-        std::cout << "Probably complaining about insignificant problem" << std::endl;
-    }
-    
+        case 0:
+            (this->*ptrs[0])();
+            break;
+        case 1:
+            (this->*ptrs[1])();
+            break;
+        case 2:
+            (this->*ptrs[2])();
+            break;
+        case 3:
+            (this->*ptrs[3])();
+            break;
+        default:
+            std::cout << "Probably complaining about insignificant problem" << std::endl;
+    } 
 }
