261df68605a2369150a5fa102bea59b33f3c5728
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-02-17 17:33:38 +0100

    handle main for tests

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 17:23:58 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/17 17:24:27 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/02/17 17:33:13 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 int main ()
 {
 
-    //test 
-
-
+    Harl harl;
+    harl.complain("warning");
+    harl.complain("info");
+    harl.complain("debug");
+    harl.complain("error");
     return (0);
 }
