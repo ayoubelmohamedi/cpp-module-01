29fe76c5407043989b591ce6a3eb72531dc645de
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-02-18 17:03:17 +0100

    fix ex00 : call zombie announce(), after new zombie creation

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 13:30:37 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/17 22:21:30 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:21:04 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(void)
 {
 	Zombie *zombie = newZombie("Buckethead Zombie");
 	randomChump("Basic Zombie");
+	zombie->announce();
 	delete zombie;
 	return (0);
 }
